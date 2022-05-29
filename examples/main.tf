77d6da5eb926da4be691da1a3b6bd01302751f2e
Oopsops <oopsops@protonmail.com>
2022-05-29 17:03:07 +0700

    update: source

--- examples/main.tf
+++ examples/main.tf
@@ -5,8 +5,8 @@ module "pcx" {
     aws.accepter  = aws.accept
   }
 
-  # source        = "git::github.com/0opsops/pcx.git//modules?ref=v1.0.0" ## using specific tags
-  source        = "git::github.com/0opsops/pcx.git//modules"
+  # source        = "git::git@github.com:0opsops/terraform-aws-pcx.git?ref=v1.0.0"    ## using specific tags
+  source        = "git::git@github.com:0opsops/terraform-aws-pcx.git"
   request_s3    = var.request_s3
   request_state = var.request_state
   accept_s3     = var.accept_s3
