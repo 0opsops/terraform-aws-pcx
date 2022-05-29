0938be3dac7ab5de27307a706ba2fe018c66bd7e
Oopsops <oopsops@protonmail.com>
2022-05-29 22:09:58 +0700

    add: docs

--- variables.tf
+++ variables.tf
@@ -1,30 +1,25 @@
 variable "request_s3" {
   type        = string
-  default     = ""
   description = "Requester S3 Bucket name"
 }
 
 variable "request_state" {
   type        = string
-  default     = ""
   description = "Requester State file name"
 }
 
 variable "accept_s3" {
   type        = string
-  default     = ""
   description = "Accepter S3 Bucket name"
 }
 
 variable "accept_state" {
   type        = string
-  default     = ""
   description = "Accepter State file name"
 }
 
 variable "accept_region" {
   type        = string
-  default     = ""
   description = "Accepter VPC region"
 }
 
@@ -33,9 +28,6 @@ variable "tags" {
   validation {
     condition     = can(var.tags["Name"])
     error_message = "At least Name tag is required!"
-  }
-  default = {
-    Name = "Account A to Account B"
   }
   description = "PCX brief description"
 }
